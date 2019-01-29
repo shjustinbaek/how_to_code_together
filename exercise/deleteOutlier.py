b816d9d134ead58e44912e468c839f3116602472
Hyunjoe Yoo <jessicanism@gmail.com>
2019-01-29 21:02:18 +0900

    hi

--- exercise/deleteOutlier.py
+++ exercise/deleteOutlier.py
@@ -7,5 +7,11 @@ def deleteOutlier(int_list):
     Returns:
         [list of int] -- [outlier deleted list]
     """
-    
+    i=0
+    modified_list = []
+
+    while i < len(int_list):
+        if 10 <= int_list[i] and int_list[i] <= 20:
+            modified_list.append(int_list[i])
+        i+=1
     return modified_list
